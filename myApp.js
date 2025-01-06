4d501f556d86a109fc2fe0a6a9ca7fb3da838979
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-07 00:48:25 +0100

    creating a Schema for Person

--- myApp.js
+++ myApp.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [String],
+})
+let Person = mongoose.model('Ivica', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
