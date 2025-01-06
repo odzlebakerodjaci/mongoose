5df4c368d1bf48626c3c14aaf117c0cfebb37989
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-06 23:02:47 +0100

    initilaize commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 let Person;
 
