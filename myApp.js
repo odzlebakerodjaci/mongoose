c88210d7829172c5dd64fc2dedbce2399d00ff6e
odzlebakerodjaci <flpmkc@gmail.com>
2025-01-11 01:27:13 +0100

    repairing mongoose model

--- myApp.js
+++ myApp.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-const Schema = mongoose.Schema;
-const personSchema = new Schema({
+const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
